db5b2646fdb59646b91dc2fb58ef29570c8b9512
Nishitbariya <85815172+Nishitbaria@users.noreply.github.com>
2024-02-07 00:20:39 +0530

    Update HeroSection component styling

--- client/components/shared/HeroSection.jsx
+++ client/components/shared/HeroSection.jsx
@@ -7,8 +7,8 @@ export default function HeroSection() {
     <section className="pt-24 bg-white dark:bg-slate-900 dark:text-white">
       <div className="px-12 mx-auto max-w-7xl">
         <div className="w-full mx-auto text-left md:w-11/12 xl:w-9/12 md:text-center">
-          <h1 className="mb-8 text-4xl font-extrabold leading-none tracking-normal text-gray-900 dark:text-white md:text-6xl md:tracking-tight">
-            <span>GlobalTalk</span>{" "}
+          <h1 className="mb-8 text-4xl font-extrabold leading-none tracking-normal text-gray-700 dark:text-white md:text-6xl md:tracking-tight">
+            <span>CulturalConnect</span>{" "}
             <span class="inline-grid">
               <span
                 class="pointer-events-none col-start-1 row-start-1 bg-[linear-gradient(90deg,theme(colors.error)_0%,theme(colors.secondary)_9%,theme(colors.secondary)_42%,theme(colors.primary)_47%,theme(colors.accent)_100%)] bg-clip-text blur-xl [transform:translate3d(0,0,0)] [-webkit-text-fill-color:transparent] before:content-[attr(data-text)] [@supports(color:oklch(0_0_0))]:bg-[linear-gradient(90deg,oklch(var(--s))_4%,color-mix(in_oklch,oklch(var(--s)),oklch(var(--er)))_22%,oklch(var(--p))_45%,color-mix(in_oklch,oklch(var(--p)),oklch(var(--a)))_67%,oklch(var(--a))_100.2%)]"
@@ -16,7 +16,7 @@ export default function HeroSection() {
                 data-text="component library"
               ></span>{" "}
               <span class="[&amp;::selection]:text-base-content relative col-start-1 row-start-1 bg-[linear-gradient(90deg,theme(colors.error)_0%,theme(colors.secondary)_9%,theme(colors.secondary)_42%,theme(colors.primary)_47%,theme(colors.accent)_100%)] bg-clip-text [-webkit-text-fill-color:transparent] [&amp;::selection]:bg-blue-700/20 [@supports(color:oklch(0_0_0))]:bg-[linear-gradient(90deg,oklch(var(--s))_4%,color-mix(in_oklch,oklch(var(--s)),oklch(var(--er)))_22%,oklch(var(--p))_45%,color-mix(in_oklch,oklch(var(--p)),oklch(var(--a)))_67%,oklch(var(--a))_100.2%)]">
-                Connect, Innovate, Unite.
+                Ignite, Unite, Conquer.
               </span>
             </span>
           </h1>
