ca16a17cac3a958ed40d5c94a8969988a9b2d1b1
Nishitbariya <85815172+Nishitbaria@users.noreply.github.com>
2024-02-06 19:10:30 +0530

    Remove commented out code in Navbar.jsx

--- client/components/shared/Navbar.jsx
+++ client/components/shared/Navbar.jsx
@@ -79,7 +79,6 @@ export default function Navbar() {
               </Link>
             ))}
 
-            {/* <a> </a> */}
             <div>
               <div
                 className="hs-dropdown"
