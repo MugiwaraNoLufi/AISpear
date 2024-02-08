b80aae467fec452b0abcae1148d9cef69fe25cb3
Nishitbariya <85815172+Nishitbaria@users.noreply.github.com>
2024-02-08 21:02:02 +0530

    Refactor LayoutButton component to use consistent quotation marks

--- client/components/components/LayoutButton/index.jsx
+++ client/components/components/LayoutButton/index.jsx
@@ -1,8 +1,8 @@
-import React, { useState } from 'react';
-import { FaTh, FaUserCircle } from 'react-icons/fa';
+import React, { useState } from "react";
+import { FaTh, FaUserCircle } from "react-icons/fa";
 
 export default function LayoutButton({ room }) {
-  const [layOut, setLayOut] = useState('grid');
+  const [layOut, setLayOut] = useState("grid");
   const [showMenu, setShowMenu] = useState(false);
 
   const handleLayOutChange = (newLayout) => {
@@ -14,22 +14,27 @@ export default function LayoutButton({ room }) {
   };
 
   return (
-    <div className="layout-button">
-      <button onClick={() => setShowMenu(!showMenu)} className="p-2 text-black hover:text-gray-700 bg-gray-200 rounded-full hover:bg-white transition duration-200">
+    <div className="reletive">
+      <button
+        onClick={() => setShowMenu(!showMenu)}
+        className="p-2 text-black transition duration-200 bg-gray-200 rounded-full hover:text-gray-700 hover:bg-white"
+      >
         <FaTh />
       </button>
       {showMenu && (
         <div className="menu">
           <div
-            className={`menu-item ${layOut === 'grid' ? 'active' : ''}`}
-            onClick={() => handleLayOutChange('grid')}
+            className={`menu-item ${layOut === "grid" ? "active" : ""}`}
+            onClick={() => handleLayOutChange("grid")}
           >
             <FaTh />
             <span>Grid</span>
           </div>
           <div
-            className={`menu-item ${layOut === 'active-speaker' ? 'active' : ''}`}
-            onClick={() => handleLayOutChange('active-speaker')}
+            className={`menu-item ${
+              layOut === "active-speaker" ? "active" : ""
+            }`}
+            onClick={() => handleLayOutChange("active-speaker")}
           >
             <FaUserCircle />
             <span>Active Speaker</span>
