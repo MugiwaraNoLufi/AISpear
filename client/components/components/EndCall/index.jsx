e42fa020f9e7ab54a46dd541dede3918f899adf0
Nishitbariya <85815172+Nishitbaria@users.noreply.github.com>
2024-02-08 15:00:16 +0530

    Refactor code for EndCall component and HighitlightText component

--- client/components/components/EndCall/index.jsx
+++ client/components/components/EndCall/index.jsx
@@ -1,9 +1,10 @@
-import React, { useEffect, useState } from 'react';
-import { fetchRecordings } from '../../api/fetchRecording';
+import React, { useEffect, useState } from "react";
+import { fetchRecordings } from "../../api/fetchRecording";
 
-import { useParams } from 'next/navigation';
-import { useRouter } from 'next/navigation';
-import { MdGetApp } from 'react-icons/md';
+import { useParams } from "next/navigation";
+import { useRouter } from "next/navigation";
+import { MdGetApp } from "react-icons/md";
+import HighitlightText from "@/components/shared/HighitlightText";
 
 export default function EndCall() {
   const router = useRouter();
@@ -11,48 +12,55 @@ export default function EndCall() {
   const { sessionId } = useParams();
 
   const redirectNewMeeting = () => {
-  router.push('/');
+    router.push("/");
   };
 
   useEffect(() => {
     fetchRecordings(sessionId)
-      .then(data => {
+      .then((data) => {
         if (data.data) {
           setRecordings(data.data);
         }
       })
-      .catch(err => {
+      .catch((err) => {
         console.log(err);
       });
   }, [sessionId]);
 
   return (
     <div className="flex flex-col items-center justify-center">
-      <div className="text-center mb-4">
-        <h2 className="text-2xl font-bold">This is an amazing meeting</h2>
+      <div className="mb-4 text-center">
+        <h1 className="mb-4 text-3xl">
+          <HighitlightText text="Thank you for joining the meeting" />
+        </h1>
         <h2 className="text-xl">I hope you have had fun with us</h2>
+        {/* Starting new button */}
         <button
           onClick={redirectNewMeeting}
-          className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded mt-4"
+          className="px-4 py-2 mt-4 font-bold text-white bg-blue-500 rounded hover:bg-blue-700"
         >
           Start new meeting
         </button>
       </div>
+
+      {/* Other components for recordings */}
       <div className="w-1/2">
-        <div className="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4">
-          <h3 className="text-lg font-semibold">
-            {recordings && recordings.length ? 'Recordings' : 'There are no recordings'}
+        <div className="px-8 pt-6 pb-8 mb-4 bg-white rounded shadow-md flex-center">
+          <h3 className="text-lg font-semibold text-center">
+            {recordings && recordings.length
+              ? "Recordings"
+              : "There are no recordings"}
           </h3>
           {recordings && (
             <ul className="list-disc">
-              {recordings.map(recording => (
+              {recordings.map((recording) => (
                 <li key={recording.id} className="mt-2">
                   Started at: {new Date(recording.createdAt).toLocaleString()}
-                  {recording.status === 'available' && (
+                  {recording.status === "available" && (
                     <a
                       href={recording.url}
                       target="_blank"
-                      className="ml-2 inline-block align-middle"
+                      className="inline-block ml-2 align-middle"
                       rel="noreferrer"
                     >
                       <MdGetApp className="text-lg" />
